7ea835e03d225f420c50a6d139b78197931ad030
Mpoki Abel <mpokiabel@gmail.com>
2022-11-10 18:56:48 +0100

    tx handler

--- arm.boot/uart.c
+++ arm.boot/uart.c
@@ -4,72 +4,6 @@
 struct cb rxcb[UART_COUNT];
 struct cb txcb[UART_COUNT];
 
-/**
- * Receive a character from the given uart, this is a non-blocking call.
- * Returns 0 if there are no character available.
- * Returns 1 if a character was read.
- */
-
-int uart_receive(int uart, unsigned char *s)
-{
-  int value;
-
-  switch (uart)
-  {
-  case 0:
-    value = cb_get(&rxcb[0], s);
-    break;
-  case 1:
-    value = cb_get(&rxcb[1], s);
-    break;
-  case 2:
-    value = cb_get(&rxcb[2], s);
-    break;
-  case 3:
-    value = cb_get(&rxcb[3], s);
-    break;
-  }
-
-  return value + 1;
-}
-
-/**
- * Sends a character through the given uart, this is a blocking call.
- * The code spins until there is room in the UART TX FIFO queue to send
- * the character.
- */
-void uart_send(int uart, unsigned char s)
-{
-  switch (uart)
-  {
-  case 0:
-    cb_put(&txcb[0], s);
-    break;
-  case 1:
-    cb_put(&txcb[1], s);
-    break;
-  case 2:
-    cb_put(&txcb[2], s);
-    break;
-  case 3:
-    cb_put(&txcb[3], s);
-    break;
-  }
-}
-
-/**
- * This is a wrapper function, provided for simplicity,
- * it sends a C string through the given uart.
- */
-void uart_send_string(int uart, const unsigned char *s)
-{
-  while (*s != '\0')
-  {
-    uart_send(uart, *s);
-    s++;
-  }
-}
-
 int uart_get_bar(int uart_id)
 {
   switch (uart_id)
@@ -85,28 +19,6 @@ int uart_get_bar(int uart_id)
     break;
   // case 3:
   //   return UART3;
-  //   break;
-  default:
-    return -1;
-    break;
-  }
-}
-
-int uart_get_IRQ_rn(int uart_id)
-{
-  switch (uart_id)
-  {
-  case 0:
-    return UART0_IRQ;
-    break;
-  case 1:
-    return UART1_IRQ;
-    break;
-  case 2:
-    return UART2_IRQ;
-    break;
-  // case 3:
-  //   return UART3_IRQ;
   //   break;
   default:
     return -1;
@@ -131,12 +43,78 @@ void tx_handler(int uart_id)
 {
   // that the handler of the TX interrupt will empty, writing the bytes to the TX FIFO, when there is room to do so.
   int uart = uart_get_bar(uart_id);
+  uint8_t byte;
   unsigned short *uart_fr = (unsigned short *)(uart + UART_FR);
   unsigned short *uart_dr = (unsigned short *)(uart + UART_DR);
   while (1)
   {
-    if ((*uart_fr & UART_TXFF) || cb_get(&txcb[uart_id], uart_dr) != -1)
+    if ((*uart_fr & UART_TXFF) || cb_get(&txcb[uart_id], &byte) != -1)
       break;
+    *uart_dr = byte;
+  }
+
+  uint16_t imsc = *(uint16_t *)(uart + UART_IMSC);
+  imsc = imsc | UART_IMSC_TXIM;
+  *(uint16_t *)(uart + UART_IMSC) = imsc;
+}
+
+/**
+ * Receive a character from the given uart, this is a non-blocking call.
+ * Returns 0 if there are no character available.
+ * Returns 1 if a character was read.
+ */
+
+int uart_receive(int uart, unsigned char *s)
+{
+  rx_handler(uart);
+  int value = cb_get(&rxcb[0], s);
+  return value + 1;
+}
+
+/**
+ * Sends a character through the given uart, this is a blocking call.
+ * The code spins until there is room in the UART TX FIFO queue to send
+ * the character.
+ */
+void uart_send(int uart, unsigned char s)
+{
+  cb_put(&txcb[uart], s);
+  tx_handler(uart);
+  // Trigger an interrupt here !!!
+}
+
+/**
+ * This is a wrapper function, provided for simplicity,
+ * it sends a C string through the given uart.
+ */
+void uart_send_string(int uart, const unsigned char *s)
+{
+  while (*s != '\0')
+  {
+    uart_send(uart, *s);
+    s++;
+  }
+}
+
+int uart_get_IRQ_rn(int uart_id)
+{
+  switch (uart_id)
+  {
+  case 0:
+    return UART0_IRQ;
+    break;
+  case 1:
+    return UART1_IRQ;
+    break;
+  case 2:
+    return UART2_IRQ;
+    break;
+  // case 3:
+  //   return UART3_IRQ;
+  //   break;
+  default:
+    return -1;
+    break;
   }
 }
 
